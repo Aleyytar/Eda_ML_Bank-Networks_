d658cc6726777635575a42a13bb211ff2562f77f
Ali Haydar Turgut <ali-turguut40@hotmail.com>
2024-03-22 17:21:14 +0300

    commit5
    first training model is LogisticRegression completed

--- Demo.py
+++ Demo.py
@@ -73,7 +73,10 @@ st = StandardScaler()
 df["balance"] = st.fit_transform(df[["balance"]])
 df["duration"] = st.fit_transform(df[["duration"]])
 
-
+# x_train: Eğitim setindeki bağımsız değişkenlerin değerleri.
+# x_test: Test setindeki bağımsız değişkenlerin değerleri.
+# y_train: Eğitim setindeki bağımlı değişkenin değerleri.
+# y_test: Test setindeki bağımlı değişkenin değerleri.
 x_train , x_test ,y_train ,y_test =train_test_split(x,y ,test_size=0.25 ,random_state= 42)
 print(x.shape)
 print(x_train.shape)
@@ -81,3 +84,31 @@ print(x_test.shape)
 print(y.shape)
 print(y_train.shape)
 print(y_test.shape)
+
+def train_evaluate_model(model, x_train, y_train, x_test,y_test):
+
+    model.fit(x_train, y_train)  #fit the model instance 
+
+
+    predictions = model.predict(x_test) # calculate predictions
+
+    #compute metrics for evaluation
+    accuracy = accuracy_score(y_test, predictions)
+    f1 = f1_score(y_test, predictions)
+    precision = precision_score(y_test, predictions)
+    recall = recall_score(y_test, predictions)
+    balanced_accuracy = balanced_accuracy_score(y_test, predictions)
+
+    #create a dataframe to visualize the results
+    eval_df = pd.DataFrame([[accuracy, f1, precision, recall, balanced_accuracy]], columns=['accuracy', 'f1_score', 'precision', 'recall', 'balanced_accuracy'])
+    return eval_df
+
+lg = LogisticRegression(penalty ="l2" ,C=0.5)
+
+results = train_evaluate_model(lg, x_train, y_train, x_test, y_test)
+
+results.index = ['LogisticRegression']
+
+results.sort_values(by='f1_score',ascending=False).style.background_gradient(cmap = sns.color_palette("ch:s=-.2,r=.6", as_cmap=True))
+
+print(results)
