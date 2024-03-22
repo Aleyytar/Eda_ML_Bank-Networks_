bc13e77b22b4bacf14de1c8890696a1a3371a333
Ali Haydar Turgut <ali-turguut40@hotmail.com>
2024-03-22 03:33:02 +0300

    Commit3
    All Data's colums reworked int after the Rework data objects , ı used the StandartScaler two colums my data

--- Demo.py
+++ Demo.py
@@ -30,6 +30,7 @@ print(df["age"].value_counts().head(15)) ## Tekrar eden yaş grupları ve tekrar
 
 sns.histplot(x="age",data=df ,color = 'rosybrown')
 ##plt.show()
+
 # Veritabanını Oku
 df = pd.read_csv("bank.csv", sep=",", encoding='utf-8')
 
@@ -42,5 +43,30 @@ sns.heatmap(numeric_df.corr(), annot=True)
 
 #DataSeti içerisinde Empty Simple sayısı 
 IsEmptySimpleCount = df.isnull().sum()/df.shape[0]
-
 print(IsEmptySimpleCount)
+
+#Veri tabanı içerisinde kolonları tek tek gezerek içerisinde bulunan nesnel (string vb.) değerleri
+#Sayısal Değerlere Dönüştürmek için 
+le = LabelEncoder()
+
+for i in df.select_dtypes('object').columns:
+    df[i] = le.fit_transform(df[i])
+
+#Data setindeki tüm columları görebilmek için
+ # Tüm sütunları göster
+pd.set_option('display.max_columns', None) 
+ # 10 satır göster
+pd.set_option('display.max_rows', 10)     # 10 satır göster
+## print(df)
+#Datasetini sayısal verilere dönüştürülmüş halini saveledik
+df.to_csv("output.csv", index=False)
+##print(df.columns)
+print(df)
+
+#StandardScaler, veri özelliklerini (örneğin, sütunlardaki değerleri) ortalama değeri 
+#0 ve standart sapması 1 olacak şekilde ölçeklendirmeye yarayan bir ölçeklendirme yöntemidir. Bu işlem, veriye aynı ölçekte bakmayı 
+#sağlar ve bazı makine öğrenimi algoritmalarının daha iyi performans göstermesine yardımcı olur.
+st = StandardScaler()
+df["balance"] = st.fit_transform(df[["balance"]])
+df["duration"] = st.fit_transform(df[["duration"]])
+print(df)
